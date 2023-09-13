2ad67a48a3a37a6bade16873afe3495bc97c2730
Beyda Nur PINARBAŞI <140889636+BeydaNurPinarbasi@users.noreply.github.com>
2023-09-14 00:08:52 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Adınız: ");
+            string ad = Console.ReadLine();
+            Console.Write("Soyadınız: ");
+            string soyad= Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("Doğum yılınız: ");
+            int yil=int.Parse (Console.ReadLine());
+            int yas = 2023 - yil;
+            Console.WriteLine("merhaba "+ ad+" "+ soyad +" siz "+ yas+ " yaşındasınız. ");
+
+        }
+    }
+}
